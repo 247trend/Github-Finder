dbfbe14a0d48cac5715797b1ef657430c2f56722
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-29 19:10:29 +0930

    add userSearch component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
         <div className="flex flex-col justify-between h-screen">
           <Navbar />
 
-          <main className="container mx-auto px-3 pd-12">
+          <main className="container mx-auto px-3 pb-12">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,12 +9,14 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_API
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loading: true,
+    loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
+    setLoading()
+
     const res = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         Authorization: `token ${GITHUB_TOKEN}`,
@@ -28,6 +30,8 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     })
   }
+
+  const setLoading = () => dispatch({type: "SET_LOADING"})
 
   return <GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>{children}</GithubContext.Provider>
 }
