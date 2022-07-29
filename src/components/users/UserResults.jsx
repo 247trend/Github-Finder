ba49f290d421d2bc4d4399b41b3779568291b9d4
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-29 15:52:32 +0930

    add loader

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useState } from "react"
+import "../styles/loader.css"
 
 const UserResults = () => {
-
   const [users, setUsers] = useState([])
-  const [loading, isLoading] = useState(true) 
+  const [loading, isLoading] = useState(true)
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchUser()
-  },[])
+  }, [])
 
   const fetchUser = async () => {
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
-      headers:{
-        Authorization: `token ${process.env.REACT_APP_GITHUB_API}`
-      }
+    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_API}`,
+      },
     })
 
     const data = await res.json()
@@ -23,16 +23,20 @@ const UserResults = () => {
 
   if (!loading) {
     return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user)=>
-        <h3>{user.login}</h3>
-      )}
-    </div>
-  )
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    )
   } else {
-    return <h3>Loading ...</h3>
+    return (
+      <div className="text-center mx-auto">
+        <span class="loader text-center mx-auto"></span>
+        <h3>Loading ...</h3>
+      </div>
+    )
   }
-  
 }
 
 export default UserResults
