a4f655b714adc40b3d7e8413c56dda2c0d201fb8
Jason Yuan <jasonyuan0130@gmail.com>
2022-07-29 17:47:30 +0930

    minor fix

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,7 +3,7 @@ const About = () => {
     <div>
       <h1 className="text-4xl mb-4">Github Finder</h1>
       <p className="mb-4 text-md font-light">
-        This is a React application to search adn see github profile details.
+        This is a React application to search and check github profile details.
       </p>
       <p className="text-sm font-light">Version: <span className="text-light">1.0.0</span></p>
     </div>
